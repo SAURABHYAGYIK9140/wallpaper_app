8324c7143a8870b43202f637b645864c29b36a9e
saurabhyagyik9140 <s18451097@gmail.com>
2026-04-18 15:58:14 +0530

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ import 'package:upgrader/upgrader.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
